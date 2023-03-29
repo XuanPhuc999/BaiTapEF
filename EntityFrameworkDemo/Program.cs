2e64d68eee5840997d7c38bd4062ad1c5afc7917
XuanPhuc1820 <xp12071997@gmail.com>
2023-03-29 17:12:14 +0700

    first commit

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            using (var db = new EntityDBEntities())
+            {
+                var select = from s in db.Students select s;
+                foreach(var data in select)
+                {
+                    Console.WriteLine("ID: {0}", data.StudentID);
+                    Console.WriteLine("Name: {0}", data.StudentName);
+                    Console.WriteLine("Gender: {0}", data.StudentGender);
+                    Console.WriteLine("Address: {0}", data.Address);
+                }
+            }
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
